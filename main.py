0bad0dcffe971c0657771817e484424d443d365a
J. W. Sheridan <james.sheridan@outlook.com>
2020-06-08 18:30:19 -0400

    Starting a project with the aim to generate harmonies for a given melody.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+melody_file = open("melody.txt", "rt") #open to read melody
+melody = melody_file.read() #string melody from melody
+melody_file.close()
+print(melody)
+for x in len(melody):
+	pass
+	
+
+#make an array with each note
